298ff56ecaeb130c31767aea2cb3b1dfaf13c22f
Alex Sendre <rares.sendre@gmail.com>
2023-08-29 19:06:18 -0500

    made a mistake and had to copy over all data from another repo for correct updates

--- api/mergedData.js
+++ api/mergedData.js
@@ -1,1 +1,28 @@
-// for merged promises
+import { deleteSingleAuthor, getAuthorBooks, getSingleAuthor } from './authorData';
+import { deleteBook, getSingleBook } from './bookData';
+
+const getBookDetails = (firebaseKey) => new Promise((resolve, reject) => {
+  getSingleBook(firebaseKey).then((bookObj) => {
+    getSingleAuthor(bookObj.author_id)
+      .then((authorObj) => resolve({ ...bookObj, authorObj }));
+  }).catch(reject);
+});
+
+const getAuthorDetails = async (firebaseKey) => {
+  const author = await getSingleAuthor(firebaseKey);
+  const books = await getAuthorBooks(author.firebaseKey);
+
+  return { ...author, books };
+};
+
+const deleteAuthorBooksRel = (firebaseKey) => new Promise((resolve, reject) => {
+  getAuthorBooks(firebaseKey).then((authorBooksArr) => {
+    const deleteBookPromises = authorBooksArr.map((book) => deleteBook(book.firebaseKey));
+
+    Promise.all(deleteBookPromises).then(() => {
+      deleteSingleAuthor(firebaseKey).then(resolve);
+    });
+  }).catch(reject);
+});
+
+export { getBookDetails, getAuthorDetails, deleteAuthorBooksRel };
--- pages/books.js
+++ pages/books.js
@@ -13,8 +13,11 @@ const showBooks = (array) => {
   renderToDOM('#add-button', btnString);
 
   let domString = '';
-  array.forEach((item) => {
-    domString += `
+  if (array.length < 1) {
+    domString += '<p>No Books Found</p>';
+  } else {
+    array.forEach((item) => {
+      domString += `
       <div class="card">
         <img class="card-img-top" src=${item.image} alt=${item.title} style="height: 400px;">
         <div class="card-body" style="height: 180px;">
@@ -26,7 +29,8 @@ const showBooks = (array) => {
             <i id="delete-book-btn--${item.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
         </div>
       </div>`;
-  });
+    });
+  }
   renderToDOM('#store', domString);
 };
 
